b3845fc316f123c07f8607a6b9792dcc9c0d801b
Bryan Sevilla <bsevilla799@insite.4cd.edu>
2025-11-13 13:35:53 -0800

    third commit

--- j/main.js
+++ j/main.js
@@ -11,7 +11,7 @@ fetch("houses.json")
     .then((response) => response.json())
     .then((data) => {
         //create a temp holder to append all the html generated inside the forEach iterator
-        let html = "container";
+        let html = "";
 
         //the argument "house" passed to the arrow function
         //holds each item in the array in turn.
@@ -33,3 +33,58 @@ fetch("houses.json")
     .catch((err) => console.log("Oops!", err));
     //this only runs if there is an error during the above process. end fetch. Log is a function. 
     //method of console object
+
+
+    //list
+      // display a list of the house members 
+  // when the select list has changed.
+  house.addEventListener('change', (e) => {
+    //get the house code from the select
+    const code = e.target.value;
+    let members = getCharacters(code);
+   
+    // DOM ref for ul to display members
+    const container = document.getElementById('characters');
+    
+    // clear out any previous member names
+    container.innerHTML = '';
+   
+    // create the li's and append to the ul
+    members.forEach((person)=> {
+      let item = document.createElement('LI');
+      item.innerText = person;
+      container.appendChild(item);
+    });
+    
+  }); //end addEventListener - change
+
+
+  //fetch color
+  document.addEventListener("DOMContentLoaded",init);
+
+  function init() {
+    const seedColor = 
+    getRandomHex();
+    fetch(
+        "https"://www.thecolorapi.com/scheme?
+        hex=" + mantis + "&mode=quad&count=4"
+    )
+  
+  .then((response) => response.json ())
+  .then((data) => {
+    let bgcolor = data.colors[0].hex.value;
+    let h1 = data.colors[1].hsl.h;
+    let s1 = data.colors[1].hsl.s;
+    let l1 = data.colors[1].hsl.l * 0.4;
+    let h3 = data.colors[3].hsl.h;
+    let s3 = data.colors[3].hsl.s;
+    let l3 = data.colors[3].hsl.l * 1.5;
+    data.colors[1].hsl.h;
+
+    document.body.style.backgroundColor = bgcolor;
+    setCSSVariable("--light", 'hsl(${h3}, ${s3}%, ${l3}%)');
+    setCSSVariable("--mid",) data.colors[2].hex.value);
+    setCSSVa
+  }
+
+  
